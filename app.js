aca3001e4cc711d9516224de3b40bcde101de3ec
jase <martinpardoluisma@gmail.com>
2016-10-16 05:30:39 +0200

    Update #2

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Geek TCG' });
+  res.render('index', { title: 'Geek Card Game' });
 });
 
 module.exports = router;
